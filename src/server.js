4e97707a8de5e8b6a24d561ac5687c92ce5c7ee8
Mirsidiq <mirzokirov2002@gmail.com>
2022-12-17 20:06:34 +0500

    test server

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import express from "express"
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send("ok")
+  res.json({
+    message:"ok"
+  })
 })
 
 app.listen(process.env.PORT || 8080,console.log('hello'))
